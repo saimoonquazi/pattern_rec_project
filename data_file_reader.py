8335b42eecddc37e9ba150cd3ce110b5ba3809fd
simon <saimoonquazi@gmail.com>
2019-05-19 01:13:50 +0300

    Now contains the feature csv file as well as reader script to read and shuffle the feature data.

---
+++ data_file_reader.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+import csv
+import random
+import numpy as np
+
+
+# In[51]:
+
+
+#Open the data file
+csv.register_dialect('myDialect',
+delimiter = '\t',
+skipinitialspace=True)
+
+#Read data and store the data
+data_table = []
+with open('features.csv', 'r') as csvFile:
+    reader = csv.reader(csvFile, dialect='myDialect')
+    data_table = list(reader)
+csvFile.close()
+
+data = np.zeros((len(data_table), len(data_table[0])))
+
+for i in range(0,len(data_table)):
+    tmp = data_table[i]
+    for j in range(0,len(tmp)):
+        data[i,j] = float(tmp[j])
+np.random.shuffle(data)
+
+features=data[:,1:]
+labels=data[:,[0]]
+
+
+# In[ ]:
+
+
+
+
